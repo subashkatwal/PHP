5453c5ce8c79ef628156f1acbcc14659d5530684
Subash Katwal <subashkatwal112@gmail.com>
2025-04-04 11:06:10 +0545

    initial commit

--- mypage.php
+++ mypage.php
@@ -1,7 +1,22 @@
 <?php
-echo "hungry bhai","Hungry girl"; //multiple parameters are displayed
+//echo "hungry bhai","Hungry girl"; //multiple parameters are displayed
 
-print "Hello World" ,"asdf"; //single parameters are displayed
+//print "Hello World" ,"asdf"; //single parameters are displayed
+
+
+$var_name="Subash";
+$var_name2= 1234;
+
+echo "Hello $var_name";
+echo "<br />";
+echo 'Hello $var_name';
+
+if ($var_name == "ram"){   //=== value + data types
+    echo"This is true";
+}
+else{
+    echo"This is false";
+}
 
 ?> 
 
